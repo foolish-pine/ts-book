4824bf8b23b6ab71027bf85e219f8e932662f2dd
Takuya Matsuda <matsuda.takuya41@gmail.com>
2022-02-01 12:46:04 +0900

    section12 途中まで

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,27 @@
+class HitAndBlow {
+  answerSource = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  answer: string[] = [];
+  tryCount = 0;
+
+  setting() {
+    const answerLength = 3;
+
+    while (this.answer.length < answerLength) {
+      const randNum = Math.floor(Math.random() * this.answerSource.length);
+      const selectedItem = this.answerSource[randNum];
+      if (!this.answer.includes(selectedItem)) {
+        this.answer.push(selectedItem);
+      }
+    }
+  }
+
+  async play() {
+    const inputArr = (
+      await promptInput("「,区切りで3つの数字を入力してください」")
+    ).split(",");
+  }
+}
+
 const printLine = (text: string, breakLine: boolean = true) => {
   process.stdout.write(text + (breakLine ? "\n" : ""));
 };
@@ -16,4 +40,10 @@ const promptInput = async (text: string) => {
   const age = await promptInput("年齢を入力してください");
   console.log(age);
   process.exit();
+})();
+
+(async () => {
+  const hitAndBlow = new HitAndBlow();
+  hitAndBlow.setting();
+  await hitAndBlow.play();
 })();
