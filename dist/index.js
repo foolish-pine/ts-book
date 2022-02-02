8685d52135b446ca69d41c48f614b73b2f736bb0
Takuya Matsuda <matsuda.takuya41@gmail.com>
2022-02-02 13:20:51 +0900

    section12途中まで

--- dist/index.js
+++ dist/index.js
@@ -35,6 +35,69 @@ var __generator = (this && this.__generator) || function (thisArg, body) {
         if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
     }
 };
+var HitAndBlow = /** @class */ (function () {
+    function HitAndBlow() {
+        this.answerSource = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+        this.answer = [];
+        this.tryCount = 0;
+    }
+    HitAndBlow.prototype.setting = function () {
+        var answerLength = 3;
+        while (this.answer.length < answerLength) {
+            var randNum = Math.floor(Math.random() * this.answerSource.length);
+            var selectedItem = this.answerSource[randNum];
+            if (!this.answer.includes(selectedItem)) {
+                this.answer.push(selectedItem);
+            }
+        }
+    };
+    HitAndBlow.prototype.play = function () {
+        return __awaiter(this, void 0, void 0, function () {
+            var inputArr, result;
+            return __generator(this, function (_a) {
+                switch (_a.label) {
+                    case 0: return [4 /*yield*/, promptInput("「,区切りで3つの数字を入力してください」")];
+                    case 1:
+                        inputArr = (_a.sent()).split(",");
+                        result = this.check(inputArr);
+                        if (!(result.hit !== this.answer.length)) return [3 /*break*/, 3];
+                        printLine("---\nHit: ".concat(result.hit, "\nBlow: ").concat(result.blow, "\n---"));
+                        this.tryCount++;
+                        return [4 /*yield*/, this.play()];
+                    case 2:
+                        _a.sent();
+                        return [3 /*break*/, 4];
+                    case 3:
+                        this.tryCount++;
+                        _a.label = 4;
+                    case 4: return [2 /*return*/];
+                }
+            });
+        });
+    };
+    HitAndBlow.prototype.check = function (input) {
+        var _this = this;
+        var hitCount = 0;
+        var blowCount = 0;
+        input.map(function (val, i) {
+            if (val === _this.answer[i]) {
+                hitCount++;
+            }
+            else if (_this.answer.includes(val)) {
+                blowCount++;
+            }
+        });
+        return {
+            hit: hitCount,
+            blow: blowCount
+        };
+    };
+    HitAndBlow.prototype.end = function () {
+        printLine("\u6B63\u89E3\u3067\u3059\uFF01 \n\u8A66\u884C\u56DE\u6570: ".concat(this.tryCount, "\u56DE"));
+        process.exit();
+    };
+    return HitAndBlow;
+}());
 var printLine = function (text, breakLine) {
     if (breakLine === void 0) { breakLine = true; }
     process.stdout.write(text + (breakLine ? "\n" : ""));
@@ -55,18 +118,16 @@ var promptInput = function (text) { return __awaiter(void 0, void 0, void 0, fun
     });
 }); };
 (function () { return __awaiter(void 0, void 0, void 0, function () {
-    var name, age;
+    var hitAndBlow;
     return __generator(this, function (_a) {
         switch (_a.label) {
-            case 0: return [4 /*yield*/, promptInput("名前を入力してください")];
+            case 0:
+                hitAndBlow = new HitAndBlow();
+                hitAndBlow.setting();
+                return [4 /*yield*/, hitAndBlow.play()];
             case 1:
-                name = _a.sent();
-                console.log(name);
-                return [4 /*yield*/, promptInput("年齢を入力してください")];
-            case 2:
-                age = _a.sent();
-                console.log(age);
-                process.exit();
+                _a.sent();
+                hitAndBlow.end();
                 return [2 /*return*/];
         }
     });
--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 class HitAndBlow {
-  answerSource = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
-  answer: string[] = [];
-  tryCount = 0;
+  private answerSource = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "0"];
+  private readonly answer: string[] = [];
+  private tryCount = 0;
 
   setting() {
     const answerLength = 3;
@@ -19,6 +19,38 @@ class HitAndBlow {
     const inputArr = (
       await promptInput("「,区切りで3つの数字を入力してください」")
     ).split(",");
+    const result = this.check(inputArr);
+
+    if (result.hit !== this.answer.length) {
+      printLine(`---\nHit: ${result.hit}\nBlow: ${result.blow}\n---`);
+      this.tryCount++;
+      await this.play();
+    } else {
+      this.tryCount++;
+    }
+  }
+
+  private check(input: string[]) {
+    let hitCount = 0;
+    let blowCount = 0;
+
+    input.map((val, i) => {
+      if (val === this.answer[i]) {
+        hitCount++;
+      } else if (this.answer.includes(val)) {
+        blowCount++;
+      }
+    });
+
+    return {
+      hit: hitCount,
+      blow: blowCount,
+    };
+  }
+
+  end() {
+    printLine(`正解です！ \n試行回数: ${this.tryCount}回`);
+    process.exit();
   }
 }
 
@@ -35,15 +67,8 @@ const promptInput = async (text: string) => {
 };
 
 (async () => {
-  const name = await promptInput("名前を入力してください");
-  console.log(name);
-  const age = await promptInput("年齢を入力してください");
-  console.log(age);
-  process.exit();
-})();
-
-(async () => {
   const hitAndBlow = new HitAndBlow();
   hitAndBlow.setting();
   await hitAndBlow.play();
+  hitAndBlow.end();
 })();
